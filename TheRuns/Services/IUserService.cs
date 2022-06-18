5c62753f81069b8425fda63795c2f47132e3ac07
Marshall Offutt <marshalloffutt@gmail.com>
2022-06-18 15:15:57 -0500

    Adds user controller and service.

---
+++ TheRuns/Services/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using TheRuns.Models;
+
+namespace TheRuns.Services
+{
+    public interface IUserService
+    {
+        User GetUser(Guid id);
+        User CreateUser(User user);
+        User UpdateUser(User user);
+        void DeleteUser(Guid id);
+    }
+}
