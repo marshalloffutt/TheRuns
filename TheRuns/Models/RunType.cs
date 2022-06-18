66b48023b9207a0f042cfff161767ca32cd7236b
Marshall Offutt <marshalloffutt@gmail.com>
2022-06-18 12:36:00 -0500

    Adds basic data models for runs, service, and controller.

---
+++ TheRuns/Models/RunType.cs
@@ -0,0 +1,8 @@
+﻿namespace TheRuns.Models
+{
+    public class RunType
+    {
+        public int Id { get; set; }
+        public string? Type { get; set; }
+    }
+}
---
+++ TheRuns/Services/RunService.cs
@@ -0,0 +1,19 @@
+﻿using TheRuns.Models;
+
+namespace TheRuns.Services
+{
+    public class RunService : IRunService
+    {
+
+        public RunService()
+        {
+            //connect to db
+        }
+
+        public RunDetails CreateRun(RunDetails run)
+        {
+            //add to db
+            return new RunDetails();
+        }
+    }
+}
